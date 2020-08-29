b3bcd3a11fae602b409a5355687db9aa9f1f5039
lifinsky <serg.lifinsky@gmail.com>
2020-08-29 14:08:10 +0300

    Add listeners for correct AMQP pool connections init, release and close.

---
+++ src/Listener/CoroutineDestoryListener.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+
+namespace Swoft\Amqp\Listener;
+
+use Swoft\Amqp\Connection\ConnectionManager;
+use Swoft\Bean\BeanFactory;
+use Swoft\Event\Annotation\Mapping\Listener;
+use Swoft\Event\EventHandlerInterface;
+use Swoft\Event\EventInterface;
+use Swoft\SwoftEvent;
+
+/**
+ * Class CoroutineDestoryListener
+ *
+ * @since 2.0
+ *
+ * @Listener(event=SwoftEvent::COROUTINE_DESTROY)
+ */
+class CoroutineDestoryListener implements EventHandlerInterface
+{
+    /**
+     * @param EventInterface $event
+     *
+     */
+    public function handle(EventInterface $event): void
+    {
+        /* @var ConnectionManager $conManager */
+        $conManager = BeanFactory::getBean(ConnectionManager::class);
+        $conManager->release(true);
+    }
+}
